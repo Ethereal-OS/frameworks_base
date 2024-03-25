e51d216c9fc169470bd942df2e478691eb9e08da
IamCOD3X <sourabh.panchal28@gmail.com>
2024-03-25 23:15:23 +0530

    Do not share key mappings with JNI object
    The key mapping information between the native key mappings and
    the KeyCharacterMap object available in Java is currently shared,
    which means that a read can be attempted while it's being modified.
    
    Bug: 274058082
    Test: Patch tested by Oppo
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:3d993de0d1ada8065d1fe561f690c8f82b6a7d4b)
    Merged-In: I745008a0a8ea30830660c45dcebee917b3913d13
    Change-Id: I745008a0a8ea30830660c45dcebee917b3913d13

--- core/jni/android_view_InputDevice.cpp
+++ core/jni/android_view_InputDevice.cpp
@@ -41,6 +41,13 @@ jobject android_view_InputDevice_create(JNIEnv* env, const InputDeviceInfo& devi
     if (!nameObj.get()) {
         return NULL;
     }
+    
+    // b/274058082: Pass a copy of the key character map to avoid concurrent
+    // access
+    std::shared_ptr<KeyCharacterMap> map = deviceInfo.getKeyCharacterMap();
+    if (map != nullptr) {
+        map = std::make_shared<KeyCharacterMap>(*map);
+    }
 
     ScopedLocalRef<jstring> descriptorObj(env,
             env->NewStringUTF(deviceInfo.getIdentifier().descriptor.c_str()));
@@ -49,8 +56,7 @@ jobject android_view_InputDevice_create(JNIEnv* env, const InputDeviceInfo& devi
     }
 
     ScopedLocalRef<jobject> kcmObj(env,
-            android_view_KeyCharacterMap_create(env, deviceInfo.getId(),
-            deviceInfo.getKeyCharacterMap()));
+            android_view_KeyCharacterMap_create(env, deviceInfo.getId(), map));
     if (!kcmObj.get()) {
         return NULL;
     }
